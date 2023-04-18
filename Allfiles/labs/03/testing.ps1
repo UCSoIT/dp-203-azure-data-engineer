b3e73b23b6821927569b2cffa6223ce4851496ab
venkatavivekghanta <ghantavenkatavivek@gmail.com>
2023-04-18 12:05:34 -0400

    UC_SoIT-Grad Topics in IT
    Updated the setup files with UCID, Locations are predefined to eastus.
    
    Co-Authored-By: dekokdj1 <70283255+dekokdj1@users.noreply.github.com>

---
+++ Allfiles/labs/03/testing.ps1
@@ -0,0 +1,70 @@
+Clear-Host
+write-host "Starting script at $(Get-Date)"
+
+Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+Install-Module -Name Az.Synapse -Force
+
+# Prompt user for a password for the SQL Database
+$sqlUser = "SQLUser"
+$sqlPassword = "ABCabc@123"
+
+$idsaved = "5425a14b-9723-4328-89bf-5bc8a3a25349"
+
+# Register resource providers
+Write-Host "Registering resource providers...";
+$provider_list = "Microsoft.Synapse", "Microsoft.Sql", "Microsoft.Storage", "Microsoft.Compute"
+foreach ($provider in $provider_list){
+    $result = Register-AzResourceProvider -ProviderNamespace $provider
+    $status = $result.RegistrationState
+    Write-Host "$provider : $status"
+}
+
+# Generate unique random suffix
+[string]$suffix =  "testingx"
+Write-Host "Your randomly-generated suffix for Azure resources is $suffix"
+$resourceGroupName = "dp203-03-$suffix"
+
+$Region = "eastus"
+
+Write-Host "Creating $resourceGroupName resource group in $Region ..."
+New-AzResourceGroup -Name $resourceGroupName -Location $Region | Out-Null
+
+# Create Synapse workspace
+$synapseWorkspace = "synapse$suffix"
+$dataLakeAccountName = "datalake$suffix"
+
+write-host "Creating $synapseWorkspace Synapse Analytics workspace in $resourceGroupName resource group..."
+write-host "(This may take some time!)"
+New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName `
+  -TemplateFile "setup.json" `
+  -Mode Complete `
+  -workspaceName $synapseWorkspace `
+  -dataLakeAccountName $dataLakeAccountName `
+  -sqlUser $sqlUser `
+  -sqlPassword $sqlPassword `
+  -uniqueSuffix $suffix `
+  -uniqueID $idsaved `
+  -Force
+
+# Make the current user and the Synapse service principal owners of the data lake blob store
+write-host "Granting permissions on the $dataLakeAccountName storage account..."
+write-host "(you can ignore any warnings!)"
+$subscriptionId = "1f550270-458f-4e93-ae8f-ea5797dfed4c"
+$userName = "ghantavk@mail.uc.edu"
+$id = (Get-AzADServicePrincipal -DisplayName $synapseWorkspace).id
+New-AzRoleAssignment -Objectid $id -RoleDefinitionName "Storage Blob Data Owner" -Scope "/subscriptions/$subscriptionId/resourceGroups/$resourceGroupName/providers/Microsoft.Storage/storageAccounts/$dataLakeAccountName" -ErrorAction SilentlyContinue;
+New-AzRoleAssignment -SignInName $userName -RoleDefinitionName "Storage Blob Data Owner" -Scope "/subscriptions/$subscriptionId/resourceGroups/$resourceGroupName/providers/Microsoft.Storage/storageAccounts/$dataLakeAccountName" -ErrorAction SilentlyContinue;
+
+# Upload files
+write-host "Loading data..."
+$storageAccount = Get-AzStorageAccount -ResourceGroupName $resourceGroupName -Name $dataLakeAccountName
+$storageContext = $storageAccount.Context
+Get-ChildItem "./data/*.csv" -File | Foreach-Object {
+    write-host ""
+    $file = $_.Name
+    Write-Host $file
+    $blobPath = "sales/csv/$file"
+    Set-AzStorageBlobContent -File $_.FullName -Container "files" -Blob $blobPath -Context $storageContext
+}
+
+write-host "Script completed at $(Get-Date)"
